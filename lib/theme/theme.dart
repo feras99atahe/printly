af643dec1180e5d9bfdb3680008ca7b5bd5cfc00
Feras <feras99ataher@gmail.com>
2024-01-25 14:32:04 +0200

    theme add

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,9 +1,23 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 final ThemeData myTheme = ThemeData(
-  primaryColor:  Color(0xFFB74093),
+  fontFamily: 'inter',
+  primaryColor: const Color(0xFFFCBA03),
   backgroundColor: Colors.white,
-
-
-
-
+  iconTheme: const IconThemeData(
+    color: Color(0xFF535353),
+    size: 50,
+  ),
+  appBarTheme: const AppBarTheme(
+      titleTextStyle: TextStyle(
+        fontFamily: 'inter',
+        fontWeight: FontWeight.normal,
+      ),
+      color: Colors.white,
+      centerTitle: true,
+      iconTheme: IconThemeData(
+        color: Color(0xFF535353),
+        size: 50,
+      )),
 );
